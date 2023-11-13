935b09cf57c0975cc37b68bacac0b6ca2a36374d
Ugonna <ugonnaobiagwu@gmail.com>
2023-11-12 19:59:05 -0500

    Initial Commit

---
+++ CatasLog/CatasLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatasLogApp.swift
+//  CatasLog
+//
+//  Created by Ugonna O on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CatasLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
